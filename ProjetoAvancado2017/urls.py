793465e070866c6d53f53a367bc8d8fb2ea10c9c
Victor Demetrio <victor.demetrio@dce.ufpb.br>
2017-10-12 16:39:32 -0300

    login-falta autenticar
    login interagindo com as outras paginas;
    o css parece ter funcionado mas falta exibir algumas imgs.

--- ProjetoAvancado2017/urls.py
+++ ProjetoAvancado2017/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^cadastro/', include('cadastro.urls')),
+    url(r'^login/', include('login.urls')),
     url(r'^home/', include('home.urls')),
 
 ]
